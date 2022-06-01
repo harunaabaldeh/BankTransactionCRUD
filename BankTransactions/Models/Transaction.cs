85538143efb71ee89f8f059d1f53a99fe2675309
harunaabaldeh <harunaabaldeh@gmail.com>
2022-06-01 14:21:14 +0100

    Bank Transaction CRUD

---
+++ BankTransactions/Models/Transaction.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [MaxLength(12, ErrorMessage ="Maximum 12 characters only")]
+        public string AccountNumber { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        public string BeneficiaryName { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        public string BankName { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only")]
+        public string SWIFTCode { get; set; }
+        [Required(ErrorMessage = "This field is required")]
+        public int Amount { get; set; }
+        [DisplayFormat(DataFormatString ="{0:MM-dd-yy}")]
+        public DateTime Date { get; set; }
+    }
+}
